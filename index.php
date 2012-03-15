aae0f0fe6dd9f021b5a84d920af646570c3ffdc4
Joe Kurleto <joedoku@gmail.com>
2012-03-14 22:05:32 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>BWillis Design</title>
+	<link href="main.css" rel="stylesheet" type="text/css" media="screen" />
+
+</head>
+	
+<body>
+					
+	<div id="navbar">
+		<a id="logo" class="selected" href="/new-site">
+			<img src="img/Header/Name_HomeLink.png" />
+		</a>
+		<a href="/new-site" class="nav_icon">
+			<img src="img/navbar/Portfolio.png" />
+		</a>
+		<a href="doc/Resume.pdf" target="_blank">
+			<img src="img/navbar/Resume.png" alt="resume" />
+		</a>
+		<a href="aboutpage.html" class="nav_icon">
+			<img src="img/navbar/About.png" />
+		</a>
+	</div>
+		
+		
+
+			<!-------------------------------- Home ------------------------------------->
+	<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box1.html">
+				<div class="label"></div>
+			</a>
+		</div>
+			
+	<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box2.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box3.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box4.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box5.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box6.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box7.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+		
+		<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box8.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box9.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+		
+			<div class="home" id="home">
+		<a href="#" class="nav_icon" >
+				<img src="img/image.png" target="box10.html">
+				<div class="label"></div>
+			</a>
+		</div>
+		
+		      		
+    
+				
+	</body>
+</html>
+
