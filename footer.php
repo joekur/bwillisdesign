a515b45806aae6c91636fa4a878a10f667e703d8
Joe Kurleto <joedoku@gmail.com>
2012-03-16 21:36:12 -0500

    added jquery. started slideshow.

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
 	
 				
 </body>
-
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/slideshow.js"></script>
 
 </html>
--- projects/one.php
+++ projects/one.php
@@ -1,5 +1,29 @@
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/header.php") ?>
 		
-My project
+
+<div class="slideshow">
+	
+	<div class="big-picture">
+		
+	</div>
+	
+	<div class="slideshow-sidebar">
+		
+		<div class="slideshow-thumbnail">
+		</div>
+		
+		<div class="slideshow-thumbnail">
+		</div>
+		
+		<div class="slideshow-thumbnail">
+		</div>
+		
+		<div class="slideshow-thumbnail">
+		</div>
+		
+	</div>
+	
+</div>
+
 		
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/footer.php") ?>
